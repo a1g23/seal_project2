010c3ef867eff538496c4de6ba4575a10210b835
Alex Greenberg <acgreenberg2@gmail.com>
2024-01-15 13:53:31 -0500

    MVP completed

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const { findOne } = require("../models/Apparel")
 
 // create the router
 
@@ -44,7 +45,47 @@ router.get("/login", (req, res) => {
     res.render("user/login.ejs")
 })
 
+// Submit Login request
+router.post("/login", async (req, res) => {
+    try {
+    // get the username and password from the body
+    const username = req.body.username
+    const password = req.body.password
 
+    // find the username in the db
+    const user = await User.findOne({username})
+    
+    // check for user, throw error if not found
+    if (!user) {
+        throw new Error("User Not Found")
+    }
+    // password verification
+    const passwordResult = await bcrypt.compare(password, user.password)
+
+    // throw error if passwordResult is false
+    if (!passwordResult) {
+        throw new Error("Password Incorrect")
+    }
+
+    // save the session
+    req.session.username = username
+    req.session.loggedIn = true
+
+    // redirect to index
+    res.redirect("/mycloset")
+
+    } catch (error) {
+    res.send("There was an Error")
+    console.log(error.message)
+    }
+})
+
+// Logout Route
+router.get("/logout", async (req, res) => {
+    await req.session.destroy((err) => {
+        res.redirect("/user/login")
+    })
+})
 
 // export the user router
 
--- server.js
+++ server.js
@@ -5,11 +5,10 @@ require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const methodOverride = require("method-override")
-const mongoose = require("mongoose")
-const mongoConnection = require("./controllers/db")
-const Apparel = require("./models/Apparel")
 const apparelController = require("./controllers/apparel")
 const userController = require("./controllers/user")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
 
 // get dotenv variable
 
@@ -25,6 +24,12 @@ app.use(morgan("dev")) // the string dev is the type of logs that morgan will se
 app.use(methodOverride("_method")) // allow for put and delete requests
 app.use(express.urlencoded({extended: true})) // parse url encoded bodys (forms that we can look reqs)
 app.use(express.static("public"))
+app.use(session({
+  secret: process.env.SECRET,
+  store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
+  saveUninitialized: true,
+  resave: false
+}))
 
 // register my routes
 
